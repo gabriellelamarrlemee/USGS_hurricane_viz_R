99f80bf6f57dcd9d58dd22f3c260813053a4bbe5
gabriellelamarrlemee <lamarrlemee.g@husky.neu.edu>
2019-01-10 08:58:02 -0600

    style changes

--- app.R
+++ app.R
@@ -99,7 +99,7 @@ ui <- bootstrapPage(
                .irs {width: 300px; float: left; display: inline-block;}
                .play {font-size: 18px !important; color: #0f284a !important;}
                .pause {font-size: 18px !important; color: #0f284a !important;}
-               .slider-animate-container {float: right; display: inline-block; height: 60px; width: 50px; margin-top: 20px !important; text-align: center !important;}
+               .slider-animate-container {float: right; display: inline-block; height: 60px; width: 50px; margin-top: 18px !important; text-align: left !important;}
                .irs-bar {width: 300px; height: 10px; background: black; border: none;}
                .irs-bar-edge {background: black; border: none; height: 10px; border-radius: 50px; width: 20px;}
                .irs-line {border: none; height: 10px; border-radius: 50px;}
@@ -118,7 +118,7 @@ ui <- bootstrapPage(
                .form-group {padding-left: 26px;}
                "),
     leafletOutput("map", width="100%", height="100vh"),
-    absolutePanel(top = 100, right = 50, fixed = TRUE,
+    absolutePanel(top = 230, right = 50, fixed = TRUE,
                   width = 400, height = 100,
                   style = "margin-left: auto;margin-right: auto;",
                   dygraphOutput("graph", width = "100%", height = "200px"),
@@ -135,27 +135,52 @@ ui <- bootstrapPage(
 
 server <- function(input, output, session) {
     
-    precipColor <- colorBin(palette = c('#edf5e5', '#bcdec9', '#aec8d5', '#8c6bb1', '#9c90ba', '#5d426d', '#361e44'),
+    precipColor <- colorBin(palette = c('#ccdb96', '#abd0a5', '#89bad3', '#7d88b5', '#67508e', '#5a396d', '#371437'),
                             bins = 7, pretty = TRUE,
                             domain = precip_merge$precip)
     
     output$map <- renderLeaflet({ # Build map
         leaflet(options = leafletOptions(zoomControl=FALSE, height="100vh")) %>%
-            addProviderTiles(providers$Esri.WorldTopoMap,
+            addMapPane(name = "polygons", zIndex = 410) %>% 
+            addMapPane(name = "maplabels", zIndex = 420) %>%
+            addProviderTiles(providers$CartoDB.PositronNoLabels,
+            # addProviderTiles(providers$Esri.WorldTopoMap,
                              options = providerTileOptions(opacity = 1)) %>%
+            addProviderTiles("CartoDB.PositronOnlyLabels", 
+                             options = leafletOptions(pane = "maplabels"),
+                             group = "map labels") %>%
             setView(lng=-76.1637, lat=33.8361, zoom=7) %>%
             addMinicharts(lng = streamdata_time_new$dec_long_va, 
                           lat = streamdata_time_new$dec_lat_va, 
                           layerId = streamdata_time_new$station_nm,
                           type = "bar", maxValues = 42, 
-                          width = 15, height = 120, fillColor = "#0f284a")
+                          width = 15, height = 120, fillColor = "#226eae")
+    })
+    
+    
+    observe({ # Add hurricane path
+        dat <- pts_wgs84[ which(pts_wgs84$dateTime <= input$time), ]
+        leafletProxy("map") %>% clearGroup("hurricane path")
+        for (i in 1:nrow(dat)) {
+            dat.sub <- dat[i:(i+1),]
+            leafletProxy("map", data = dat.sub) %>%
+                addPolylines(lng=~LON, lat=~LAT, color="#aababf", 
+                             opacity=1, layerId=paste('hurricanePath',i,sep="_"), 
+                             options = leafletOptions(pane = "polygons"),
+                             group = "hurricane path",
+                             weight=~ifelse(STORMTYPE == 'DB', 1, 
+                                           ifelse(STORMTYPE == 'LO', 5,
+                                                  ifelse(STORMTYPE =='TD', 10,
+                                                         ifelse(STORMTYPE == 'TS', 15, 30)))))
+        }
     })
     
     observe({ # Add precip polygons
         leafletProxy("map", data = subset(precip_merge, time == input$time)) %>%
             removeShape(s) %>%
             addPolygons(color = ~precipColor(precip), weight = 0, 
-                        smoothFactor = 0.5, opacity = 1.0, fillOpacity = 0.75, layerId=~id)
+                        smoothFactor = 0.5, opacity = 0.45, fillOpacity = 0.45, layerId=~id,
+                        options = leafletOptions(pane = "polygons"))
         s <<- subset(precip_merge, time == input$time)$id
     })
     
@@ -169,14 +194,6 @@ server <- function(input, output, session) {
             )
     })
     
-    observe({ # Add hurricane path
-        dat <- subset(pts_wgs84, dateTime <= input$time)
-        d <- points_to_line(dat, "LON", "LAT", sort_field = "dateTime")
-        leafletProxy("map", data = d) %>%
-            addPolylines(color="#0f284a", opacity=0.25, layerId='hurricanePath')
-            # addPolylines(color="#0f284a", opacity=1, weight=~LON/10)
-    })
-    
     observe({
         input$time # Update the time series to align with the map
         updated <- stream_ts[paste('2018/',input$time,sep="")]
@@ -187,34 +204,34 @@ server <- function(input, output, session) {
                 dyRangeSelector(dateWindow = c("2018-09-13 00:00:00", "2018-09-19 11:00:00"), height = 20) %>%
                 dyLegend(show="never") %>%
                 dyOptions(drawGrid = FALSE) %>%
-                dyLimit(limit=0, label = "flooding", labelLoc = "left",
+                dyLimit(limit=0, label = "flooding level", labelLoc = "left",
                         color = "black", strokePattern = "dashed") %>%
                 # dyShading(from = "-20", to = "0", color = "#EFEFEF", axis = "y") %>%
                 dyHighlight(highlightCircleSize = 0, highlightSeriesBackgroundAlpha = 1)  %>%
-                # Below 0 is gray
-                dySeries("2096500B", color = "gray") %>%
-                dySeries("2096960B", color = "gray") %>%
-                dySeries("2100500B", color = "gray") %>%
-                dySeries("2102000B", color = "gray") %>%
-                dySeries("2102500B", color = "gray") %>%
-                dySeries("2103000B", color = "gray") %>%
-                dySeries("2104000B", color = "gray") %>%
-                dySeries("2105769B", color = "gray") %>%
-                dySeries("2106500B", color = "gray") %>%
-                dySeries("2108000B", color = "gray") %>%
-                dySeries("2108566B", color = "gray") %>%
-                # Above 0 is black
-                dySeries("2096500A", color = "black") %>%
-                dySeries("2096960A", color = "black") %>%
-                dySeries("2100500A", color = "black") %>%
-                dySeries("2102000A", color = "black") %>%
-                dySeries("2102500A", color = "black") %>%
-                dySeries("2103000A", color = "black") %>%
-                dySeries("2104000A", color = "black") %>%
-                dySeries("2105769A", color = "black") %>%
-                dySeries("2106500A", color = "black") %>%
-                dySeries("2108000A", color = "black") %>%
-                dySeries("2108566A", color = "black") 
+                # Below 0 is #a4aeb6
+                dySeries("2096500B", color = "#aababf", strokePattern = "dotted") %>%
+                dySeries("2096960B", color = "#aababf", strokePattern = "dotted") %>%
+                dySeries("2100500B", color = "#aababf", strokePattern = "dotted") %>%
+                dySeries("2102000B", color = "#aababf", strokePattern = "dotted") %>%
+                dySeries("2102500B", color = "#a4aeb6", strokePattern = "dotted") %>%
+                dySeries("2103000B", color = "#a4aeb6", strokePattern = "dotted") %>%
+                dySeries("2104000B", color = "#a4aeb6", strokePattern = "dotted") %>%
+                dySeries("2105769B", color = "#a4aeb6", strokePattern = "dotted") %>%
+                dySeries("2106500B", color = "#a4aeb6", strokePattern = "dotted") %>%
+                dySeries("2108000B", color = "#a4aeb6", strokePattern = "dotted") %>%
+                dySeries("2108566B", color = "#a4aeb6", strokePattern = "dotted") %>%
+                # Above 0 is #0f284a
+                dySeries("2096500A", color = "#0f284a") %>%
+                dySeries("2096960A", color = "#0f284a") %>%
+                dySeries("2100500A", color = "#0f284a") %>%
+                dySeries("2102000A", color = "#0f284a") %>%
+                dySeries("2102500A", color = "#0f284a") %>%
+                dySeries("2103000A", color = "#0f284a") %>%
+                dySeries("2104000A", color = "#0f284a") %>%
+                dySeries("2105769A", color = "#0f284a") %>%
+                dySeries("2106500A", color = "#0f284a") %>%
+                dySeries("2108000A", color = "#0f284a") %>%
+                dySeries("2108566A", color = "#0f284a") 
     
         })
     })
@@ -225,49 +242,7 @@ server <- function(input, output, session) {
 shinyApp(ui, server)
 
 
-points_to_line <- function(data, long, lat, id_field = NULL, sort_field = NULL) {
-    
-    data <- data.frame(data)
-    
-    # Convert to SpatialPointsDataFrame
-    coordinates(data) <- c(long, lat)
-    
-    # If there is a sort field...
-    if (!is.null(sort_field)) {
-        if (!is.null(id_field)) {
-            data <- data[order(data[[id_field]], data[[sort_field]]), ]
-        } else {
-            data <- data[order(data[[sort_field]]), ]
-        }
-    }
-    
-    # If there is only one path...
-    if (is.null(id_field)) {
-        
-        lines <- SpatialLines(list(Lines(list(Line(data)), "id")))
-        
-        return(lines)
-        
-        # Now, if we have multiple lines...
-    } else if (!is.null(id_field)) {  
-        
-        # Split into a list by ID field
-        paths <- sp::split(data, data[[id_field]])
-        
-        sp_lines <- SpatialLines(list(Lines(list(Line(paths[[1]])), "line1")))
-        
-        # I like for loops, what can I say...
-        for (p in 2:length(paths)) {
-            id <- paste0("line", as.character(p))
-            l <- SpatialLines(list(Lines(list(Line(paths[[p]])), id)))
-            sp_lines <- spRbind(sp_lines, l)
-        }
-        
-        return(sp_lines)
-    }
-}
 
 # NOTES
-# Add colors to the precip data
 # Add a key for the precip data colors
 # Add a key for the hurricane path width data
